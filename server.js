7491000f667193d8717d7604666e5068db795a10
Roger Hui <roger.hui@gmail.com>
2017-08-26 11:25:37 +0800

    Add PUT todos

--- server.js
+++ server.js
@@ -73,7 +73,33 @@ app.delete('/todos/:id', function(req, res) {
 	}
 }); 
 
+// PUT 
 
+app.put('/todos/:id', function(req, res) {
+	var TodoId = parseInt(req.params.id, 10); 
+	var matchedTodo = _.findWhere(todos, {"id": TodoId});
+	var body = _.pick(req.body, "description", "completed", "id");
+	var validAttributes = {};
+
+	if (!matchedTodo){
+		return res.status(404).send(); 
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed=body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description=body.description.trim(); 
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send(); 
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo); 
+});
 
 
 
